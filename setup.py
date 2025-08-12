5971bc3eb0eaa2ac86cc36cbe6d214aef67883a1
manishity11 <160490232+manishity11@users.noreply.github.com>
2025-08-12 22:54:54 +0530

    Initial commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="manish",
+    author_email="manish.ee88@gmail.com",
+    packages=find_packages()
+)
